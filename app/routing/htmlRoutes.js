fd9b9949867126b9e9228fd7687afd13ba051ff8
itsmegh <42684917+itsmegh@users.noreply.github.com>
2019-01-07 16:10:15 -0700

    heroku deployment update

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -4,11 +4,11 @@ var path = require("path");
 //GET route to survey which displays the survey page
 module.exports = function(app) {
 app.get("/survey", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../survey.html")); //absolute path to the directory and absolute path for a url
+    res.sendFile(path.join(__dirname, "public", "survey.html")); //absolute path to the directory and absolute path for a url
   });
   
 //default route that leads to home.html
-  app.get("/", function(req, res) {
-    res.sendFile(path.join(__dirname, "/../home.html"));
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "public", "home.html"));
   });
 };
